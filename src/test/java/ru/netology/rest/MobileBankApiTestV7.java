b95576fe38bf33d4ebdf285d3856c5d423e4a117
artem <artem.gaponov.2024@mail.ru>
2026-01-04 15:26:12 +0300

    Добавлена проверка

--- src/test/java/ru/netology/rest/MobileBankApiTestV7.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -1,5 +1,10 @@
 package ru.netology.rest;
 
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Test;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
@@ -7,21 +12,26 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
 import static io.restassured.RestAssured.given;
 public class MobileBankApiTestV7 {
+    private static final Log log = LogFactory.getLog(MobileBankApiTestV7.class);
+
     // Given - When - Then
 // Предусловия
     @Test
     void shouldReturnDemoAccounts() {
+        var text = "Hi";
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body(text) // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("data", equalTo(text));
         ;
     }
 }
