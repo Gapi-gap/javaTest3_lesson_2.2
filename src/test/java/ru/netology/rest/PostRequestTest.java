4ed5a021e8083293e7df2893a59ebbfb005d9112
artem <artem.gaponov.2024@mail.ru>
2026-01-06 15:05:00 +0300

    Удалены прошлые тесты

--- src/test/java/ru/netology/rest/PostRequestTest.java
+++ src/test/java/ru/netology/rest/PostRequestTest.java
@@ -1,21 +1,11 @@
 package ru.netology.rest;
 
-import io.restassured.matcher.ResponseAwareMatcher;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.jupiter.api.Test;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static io.restassured.RestAssured.given;
 
-public class MobileBankApiTestV7 {
+public class PostRequestTest {
 
     // Given - When - Then
 // Предусловия
